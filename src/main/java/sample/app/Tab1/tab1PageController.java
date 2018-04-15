c892f8339961e82a7e708a64bca7fe49cf74704b
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-04-15 23:52:38 +0200

    init

--- src/main/java/sample/app/Tab1/tab1PageController.java
+++ src/main/java/sample/app/Tab1/tab1PageController.java
@@ -1,10 +1,12 @@
 package sample.app.Tab1;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTreeTableView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.*;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,25 +16,132 @@ import java.util.ResourceBundle;
  */
 public class tab1PageController implements Initializable {
 
-
-//    @FXML
-//    private Button button;
-//
-//    @FXML
-//    void buttonAction(ActionEvent event) {
-//        System.out.println("ButtonClick");
-//    }
+    @FXML
+    private ComboBox bankName;
 
     @FXML
-    private ComboBox clientName;
+    private Button addTwoBank;
 
     @FXML
-    private Button addclient;
+    private JFXRadioButton bankSahb;
 
     @FXML
-    void addclientAction(ActionEvent event) {
+    private JFXRadioButton bankEda;
+
+    @FXML
+    private TextArea bankNotes;
+
+    @FXML
+    private DatePicker bankDate;
+
+    @FXML
+    private JFXButton addBank;
+
+    @FXML
+    private JFXButton updateBank;
+
+    @FXML
+    private JFXTreeTableView bankTable;
+
+    @FXML
+    private TextField bankCurrentMoney;
+
+    @FXML
+    private JFXButton deleteBank;
+
+    @FXML
+    private JFXButton searchBank;
+
+    @FXML
+    private ComboBox<?> searchBankName;
+
+    @FXML
+    private DatePicker searchToBank;
+
+    @FXML
+    private DatePicker searchFromBank;
+
+    @FXML
+    private Tab moneyTable;
+
+    @FXML
+    private JFXRadioButton moneySahb;
+
+    @FXML
+    private JFXRadioButton moneyEda;
+
+    @FXML
+    private TextArea moneyNotes;
+
+    @FXML
+    private JFXButton addMoney;
+
+    @FXML
+    private JFXButton updateMoney;
+
+    @FXML
+    private TextField moneyMoney;
+
+    @FXML
+    private JFXButton searchMoney;
+
+    @FXML
+    private DatePicker searchToMoney;
+
+    @FXML
+    private DatePicker searchFromMoney;
+
+    @FXML
+    private TextField moneyCurrentMoney;
+
+    @FXML
+    private JFXButton deleteMoney;
+
+    @FXML
+    void addBankAction(ActionEvent event) {
 
     }
+
+    @FXML
+    void addMoneyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addTwoBankAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteBankAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteMoneyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchBankAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchMoneyAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateBankAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateMoneyAction(ActionEvent event) {
+
+    }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
