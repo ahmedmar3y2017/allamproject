d8ec74662b6db88ec02e0b801099bcc8d7991507
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-04-23 12:43:12 +0200

    init

---
+++ src/main/java/sample/app/nakl/naklController.java
@@ -0,0 +1,59 @@
+package sample.app.nakl;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by ahmed mar3y on 23/04/2018.
+ */
+public class naklController implements Initializable {
+
+
+    @FXML
+    private BorderPane holderPane;
+
+    @FXML
+    private JFXButton update;
+
+    @FXML
+    private JFXButton save;
+
+    @FXML
+    private JFXButton deleteTable;
+
+    @FXML
+    private JFXButton updateTable;
+
+    @FXML
+    void deleteTableAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateTableAction(ActionEvent event) {
+
+    }
+
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
