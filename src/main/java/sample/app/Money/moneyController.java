44a4386fe086e0f5e6f9f599b1c85112aecc53d6
Ashraf <ashrafzidan996@gmail.com>
2018-04-26 03:30:06 -0800

    desgin

--- src/main/java/sample/app/Money/moneyController.java
+++ src/main/java/sample/app/Money/moneyController.java
@@ -1,6 +1,15 @@
 package sample.app.Money;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.TreeTableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,8 +18,46 @@ import java.util.ResourceBundle;
  * Created by ahmed mar3y on 22/04/2018.
  */
 public class moneyController implements Initializable {
+    @FXML
+    private HBox hbox1 , lasthbox;
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private Pane infoPane;
+
+    @FXML
+    private TreeTableView table;
+    @FXML
+    private TreeTableColumn Col_phone , Col_Address , Col_name;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        // --------------- set size ---------------------------
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        double screenWidth = primaryScreenBounds.getWidth()-180 ;
+        hbox1.setPrefWidth(screenWidth);
+
+        tabPane.setPrefWidth(primaryScreenBounds.getWidth() - 185);
+        tabPane.setPrefHeight(primaryScreenBounds.getHeight());
+        tabPane.setTabMinWidth(screenWidth / 2 );
+//
+        infoPane.setPrefWidth(screenWidth / 3);
+        infoPane.setLayoutY(primaryScreenBounds.getHeight() / 2  - 300);
+
+//        table.setLayoutX(infoPane.getPrefWidth() );
+        table.setPrefHeight(primaryScreenBounds.getHeight() - 270);
+
+
+
+//        lasthbox.setLayoutX( screenWidth / 2 );
+        lasthbox.setLayoutY(primaryScreenBounds.getHeight() - 500);
+
+
+
+
 
     }
 }
